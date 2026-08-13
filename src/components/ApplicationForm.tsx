6855a4547a153fd6b80df5ddcb88cd4d03c165bb
mouadbt <160415648+mouadbt@users.noreply.github.com>
2026-08-13 23:31:42 +0100

    build: start building job application field

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import { useState } from "react";
 
+type jobStatus =
+  | "Ghosted"
+  | "HR Interview"
+  | "Technical Interview"
+  | "Assessment"
+  | "ghosted"
+  | "Rejected";
+
+type ApplicationFormFileds  = {
+  jobTitle: string;
+  jobDescription: string;
+  jobUrl: string;
+  jobStatus: jobStatus;
+  company: string;
+  notes: string;
+  resumeUsed: string;
+};
+const statusList: string[] = [
+  "Ghosted",
+  "HR Interview",
+  "Technical Interview",
+  "Assessment",
+  "ghosted",
+  "Rejected",
+];
+const specialFields: string[] = ["jobStatus", "notes", "resumeUsed"];
 export default function ApplicationForm() {
-  return <div>ApplicationForm</div>;
+  const [data, setData] = useState<ApplicationFormFileds>([
+    jobTitle: "",
+    jobDescription: "",
+    jobUrl: "",
+    company: "",
+    status: "ghosted",
+    notes: "",
+    resumeUsed: "",
+  ]);
+  return <form action="">{data.map}</form>;
 }
